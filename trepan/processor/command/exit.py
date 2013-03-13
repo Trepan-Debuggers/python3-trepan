2c1f7fa590c8f545b41a2817ec2f9ced7fdbf6ef
Rocky Bernstein <rocky@gnu.org>
2013-03-13 00:04:26 -0400

    Add exit command.

---
+++ trepan/processor/command/exit.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+#  Copyright (C) 2009, 2013 Rocky Bernstein
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import os
+from import_relative import import_relative
+
+# Our local modules
+Mbase_cmd  = import_relative('base_cmd', top_name='trepan')
+Mcmdfns    = import_relative('cmdfns', '..', 'trepan')
+
+class ExitCommand(Mbase_cmd.DebuggerCommand):
+    """**exit** [*exitcode*]
+
+Hard exit of the debugged program.
+
+The program being debugged is exited via *sys.exit()*. If a return code
+is given, that is the return code passed to *sys.exit()*, the
+return code that will be passed back to the OS."""
+
+    category      = 'support'
+    min_args      = 0
+    max_args      = 1
+    name          = os.path.basename(__file__).split('.')[0]
+    need_stack    = False
+    short_help    = 'Exit program via sys.exit()'
+
+    def run(self, args):
+        self.core.stop()
+        self.core.execution_status = 'Exit command'
+        if len(args) <= 1:
+            exit_code = 0
+        else:
+            exit_code = self.proc.get_int(args[1], default=0, cmdname='exit')
+            if exit_code is None: return False
+            pass
+        # FIXME: allow setting a return code.
+        import sys
+        sys.exit(int(exit_code))
+        # Not reached
+        return True
+
+# Demo it
+if __name__ == '__main__':
+    mock = import_relative('mock')
+    d, cp = mock.dbg_setup()
+    command = ExitCommand(cp)
+    command.run(['exit', 'wrong', 'number', 'of', 'args'])
+    command.run(['exit', 'foo'])
+    command.run(['exit', '10'])
