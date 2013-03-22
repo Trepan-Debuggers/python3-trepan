40cb171bf0fbb2c6e0e1068f23697d431fc4496a
Rocky Bernstein <rocky@gnu.org>
2013-03-22 18:25:16 -0400

    Add another test

---
+++ test/unit/test-except.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'Unit test for trepan.exception'
+import unittest
+from import_relative import import_relative
+
+Mexcept = import_relative('exception', '...trepan')
+
+class TestDeguggerExcept(unittest.TestCase):
+
+    def test_debugger_restart(self):
+        try:
+            raise Mexcept.DebuggerRestart(['a', 'b'])
+        except Mexcept.DebuggerRestart:
+            import sys
+            self.assertEqual(['a', 'b'], sys.exc_info()[1].sys_argv)
+        else:
+            self.assertFalse(True)
+        pass
+        return
+        
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/unit/test-info-file.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+'Unit test for trepan.clifns'
+import inspect, os, sys, unittest
+from import_relative import import_relative
+
+debugger  = import_relative('debugger', '...trepan', 'trepan')
+Minfo     = import_relative('trepan.processor.command.info', '...')
+MinfoFile = import_relative('trepan.processor.command.info_subcmd.file', 
+                            '...')
+Mdebugger = import_relative('debugger', '...trepan')
+
+from cmdhelper import dbg_setup
+
+class TestInfoFile(unittest.TestCase):
+
+    # FIXME: put in a more common place
+    # Possibly fix up Mock to include this
+    def setup_io(self, command):
+        self.clear_output()
+        command.msg = self.msg
+        command.errmsg = self.errmsg
+        command.msg_nocr = self.msg_nocr
+        return
+
+    def clear_output(self):
+        self.msgs = []
+        self.errmsgs = []
+        self.last_was_newline = True
+        return
+
+    def msg_nocr(self, msg):
+        if len(self.msgs) > 0:
+            self.msgs[-1] += msg
+        else:
+            self.msgs += msg
+            pass
+        return
+    def msg(self, msg):
+        self.msgs += [msg]
+        return
+
+    def errmsg(self, msg):
+        self.errmsgs.append(msg)
+        pass
+
+    def test_info_file(self):
+        d = Mdebugger.Trepan()
+        d, cp = dbg_setup(d)
+        command = Minfo.InfoCommand(cp, 'info')
+
+        sub = MinfoFile.InfoFile(command)
+        self.setup_io(sub)
+        sub.run([])
+        self.assertEqual([], self.msgs)
+        cp.curframe = inspect.currentframe()
+        for width in (80, 200):
+            # sub.settings['width'] = width
+            sub.run(['test-info-file.py', 'lines'])
+            sub.run([])
+            pass
+        pass
+        
+if __name__ == '__main__':
+    unittest.main()
