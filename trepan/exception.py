962ff674dfbc4ae8f3c8c5951edd8407a8990a3b
Rocky Bernstein <rocky@gnu.org>
2014-04-21 11:31:29 -0400

    Another small doc change

--- trepan/exception.py
+++ trepan/exception.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#   Copyright (C) 2008-2009, 2013 Rocky Bernstein <rocky@gnu.org>
+#   Copyright (C) 2008-2009, 2013-2014 Rocky Bernstein <rocky@gnu.org>
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -14,11 +14,11 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 class DebuggerQuit(Exception):
-    """Exception to signal graceful debugger termination"""
+    """An exception to signal a graceful termination of the program"""
     pass
 
 class DebuggerRestart(Exception):
-    """Exception to signal a (soft) program restart.
+    """An exception to signal a (soft) program restart.
     You can pass in an array containing the arguments to restart, should
     we have to issue an execv-style restart.
     """
