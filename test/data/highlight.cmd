6b141702f6ef2c7086f9f7e9652e8be66307551f
rocky <rocky@gnu.org>
2015-08-02 00:10:07 -0400

    Get ready for release 0.5.0

--- test/data/highlight.cmd
+++ test/data/highlight.cmd
@@ -1,16 +1,14 @@
 #
-# Test of the debugger 'macro' command
+# Test of the debugger 'highlight' command
 #
 set basename on
 set trace on
 set skip off
 show highlight
 set highlight
-set highlight reset
 set highlight off
 set highlight plain
 set highlight dark
 set highlight light
-set highlight light reset
 quit
 quit
--- trepan/VERSION.py
+++ trepan/VERSION.py
@@ -4,4 +4,4 @@
 # This file should define a variable VERSION which we use as the
 # debugger version number.
 
-VERSION='0.4.9'
+VERSION='0.5.0'
