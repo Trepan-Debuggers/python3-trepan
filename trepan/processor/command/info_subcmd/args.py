0a3733b6d156da035db3789303aed3f60d33d78e
rocky <rocky@gnu.org>
2016-01-08 20:14:33 -0500

    info args fails when not implicit string conversion exists

--- trepan/processor/command/info_subcmd/args.py
+++ trepan/processor/command/info_subcmd/args.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#   Copyright (C) 2008-2009, 2013, 2015 Rocky Bernstein <rocky@gnu.org>
+#   Copyright (C) 2008-2009, 2013, 2015-2015 Rocky Bernstein <rocky@gnu.org>
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -54,7 +54,7 @@ See also:
                 name = co.co_varnames[i]
                 self.msg_nocr("%d: %s = " % (i+1, name))
                 if name in d:
-                    self.msg(d[name])
+                    self.msg(str(d[name]))
                 else:
                     self.ermsg("undefined")
                     pass
