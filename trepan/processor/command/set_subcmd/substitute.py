6c3513dd27b441c235ae131cdc71657abca72b35
rocky <rocky@gnu.org>
2016-12-23 21:34:20 -0500

    Set substitute doc fixes

--- trepan/processor/command/set_subcmd/substitute.py
+++ trepan/processor/command/set_subcmd/substitute.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#   Copyright (C) 2015 Rocky Bernstein
+#   Copyright (C) 2015, 2016 Rocky Bernstein
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -18,20 +18,19 @@ import pyficache
 
 # Our local modules
 from trepan.processor.command import base_subcmd as Mbase_subcmd
-from trepan.lib import complete as Mcomplete
 
 
 class SetSubstitute(Mbase_subcmd.DebuggerSubcommand):
-    """**set substitute** **from-name** **to-path*
+    """**set substitute** *from-name* *to-path*
 
-Add a substitution rule replacing FROM into TO in source file names.
-If a substitution rule was previously set for FROM, the old rule
+Add a substitution rule replacing *from-name* into *to-path* in source file names.
+If a substitution rule was previously set for *from-name*, the old rule
 is replaced by the new one.
 
-Spaces in "filesnames" like <frozen importlib._bootstrap> messes up our normal shell
-tokenization, so we have added a hack to ignore <frozen .. >.
+Spaces in "filenames" like `<frozen importlib._bootstrap>` messes up our normal shell
+tokenization, so we have added a hack to ignore `<frozen .. >`.
 
-So for frozen files like <frozen importlib._bootstrap>, use importlib._bootstrap
+So, for frozen files like `<frozen importlib._bootstrap>`, `use importlib._bootstrap`
 
 Examples:
 --------
@@ -39,9 +38,7 @@ Examples:
     set substitute importlib._bootstrap /usr/lib/python3.4/importlib/_bootstrap.py
     set substitute ./gcd.py /tmp/gcd.py
 
-See also:
----------
-`show substitute`"""
+"""
 
     in_list    = True
     min_abbrev = len('sub')
