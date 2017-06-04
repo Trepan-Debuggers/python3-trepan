f4100d7109ea347f422a64a035c55f79005c20c4
rocky <rocky@gnu.org>
2017-06-03 23:07:13 -0400

    Get ready for release 0.7.5

--- setup.py
+++ setup.py
@@ -56,7 +56,8 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 long_description   = ( read("README.rst") + '\n' )
 
-from setuptools import setup
+from setuptools import setup, find_packages
+packages = find_packages() + ['trepan.api']
 
 setup(
        author             = author,
