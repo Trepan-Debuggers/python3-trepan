d78b2b988ee1a3f8eb2ce435c0eec94d0b629d02
rocky <rocky@gnu.org>
2018-01-27 14:22:26 -0500

    Bump xdis again

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -50,7 +50,7 @@ install_requires   = ['columnize >= 0.3.9',
                       'spark_parser >= 1.8.5, <1.9.0',
                       'tracer >= 0.3.2',
                       'uncompyle6 >= 2.15.0',
-                      'xdis >= 3.6.8, < 3.7.0',
+                      'xdis >= 3.6.9, < 3.7.0',
                       ]
 license            = 'GPL3'
 mailing_list       = 'python-debugger@googlegroups.com'
