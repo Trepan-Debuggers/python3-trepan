8229e43a1e3a2f27ac27f7d6b33ee3bc5cc9d7c7
rocky <rocky@gnu.org>
2019-04-09 21:46:02 -0400

    Bump required xdis, uncompyle6 versions

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -49,8 +49,8 @@ install_requires   = ['columnize >= 0.3.9',
                       'pygments  >= 2.2.0',
                       'spark_parser >= 1.8.7, <1.9.0',
                       'tracer >= 0.3.2',
-                      'uncompyle6 >= 3.2.4',
-                      'xdis >= 3.8.8',
+                      'uncompyle6 >= 3.2.6',
+                      'xdis >= 4.0.0, < 4.1.0',
                       ]
 license            = 'GPL3'
 mailing_list       = 'python-debugger@googlegroups.com'
