da8c3c937f8c2764bac774243e7d1299f0910a8f
rocky <rocky@gnu.org>
2019-08-11 17:12:24 -0400

    Go with Pygments 2.2.0...
    This seems to have the colors we want.
    
    Try 3.7 travis testing

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -46,10 +46,10 @@ ftp_url            = None
 install_requires   = ['columnize >= 0.3.9',
                       'nose>=1.0.0, <= 1.3.7',
                       'pyficache >= 1.0.0',
-                      'pygments  >= 2.2.0',
+                      'pygments == 2.2.0',
                       'spark_parser >= 1.8.7, <1.9.0',
                       'tracer >= 0.3.2',
-                      'uncompyle6 >= 3.2.6',
+                      'uncompyle6 >= 3.3.5',
                       'xdis >= 4.0.0, < 4.1.0',
                       ]
 license            = 'GPL3'
