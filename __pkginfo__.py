960543738bb81717d30cca186f6d21c01d8e2eef
rocky <rocky@gnu.org>
2020-04-17 00:20:41 -0400

    TravisCI 2nd try

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -53,13 +53,13 @@ author = "Rocky Bernstein"
 author_email = "rocky@gnu.org"
 ftp_url = None
 install_requires = [
-    "columnize >= 0.3.9",
+    "columnize >= 0.3.10",
     "nose>=1.0.0, <= 1.3.7",
     "pyficache >= 1.0.0",
     "pygments %s" % pygments_version,
     "spark_parser >= 1.8.9, <1.9.0",
     "tracer >= 0.3.2",
-    "uncompyle6 >= 3.6.5",
+    # "uncompyle6 >= 3.6.5",
     "xdis >= 4.3.2, < 4.4.0",
 ]
 license = "GPL3"
