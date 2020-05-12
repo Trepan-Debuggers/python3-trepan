13fabf2ca0a422c09cc03d5817ca9202a6f737d6
rocky <rocky@gnu.org>
2020-05-11 23:47:59 -0400

    Blaken another buffer

--- trepan/processor/command/info_subcmd/pc.py
+++ trepan/processor/command/info_subcmd/pc.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#   Copyright (C) 2017 Rocky Bernstein <rocky@gnu.org>
+#   Copyright (C) 2017, 2020 Rocky Bernstein <rocky@gnu.org>
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -18,11 +18,12 @@ import inspect
 from dis import findlinestarts
 
 # Our local modules
-from trepan.processor.command import base_subcmd as Mbase_subcmd
+from trepan.processor.command.base_subcmd import DebuggerSubcommand
 from trepan.lib.disassemble import disassemble_bytes
-from trepan import misc as Mmisc
+from trepan.misc import wrapped_lines
 
-class InfoPC(Mbase_subcmd.DebuggerSubcommand):
+
+class InfoPC(DebuggerSubcommand):
     """**info pc**
 
 List the current program counter or bytecode offset,
@@ -35,9 +36,9 @@ See also:
 """
 
     min_abbrev = 2  # Need at least info 'pc'
-    max_args   = 0
+    max_args = 0
     need_stack = True
-    short_help = 'Show Program Counter or Instruction Offset information'
+    short_help = "Show Program Counter or Instruction Offset information"
 
     def run(self, args):
         """Program counter."""
@@ -46,35 +47,46 @@ See also:
             curframe = self.proc.curframe
             if curframe:
                 line_no = inspect.getlineno(curframe)
-                offset  = curframe.f_lasti
+                offset = curframe.f_lasti
                 self.msg("PC offset is %d." % offset)
                 offset = max(offset, 0)
                 code = curframe.f_code
                 co_code = code.co_code
-                disassemble_bytes(self.msg, self.msg_nocr,
-                                  co_code, offset, line_no, line_no-1, line_no+1,
-                                  constants=code.co_consts, cells=code.co_cellvars,
-                                  varnames=code.co_varnames, freevars=code.co_freevars,
-                                  linestarts=dict(findlinestarts(code)),
-                                  end_offset=offset+10)
+                disassemble_bytes(
+                    self.msg,
+                    self.msg_nocr,
+                    co_code,
+                    offset,
+                    line_no,
+                    line_no - 1,
+                    line_no + 1,
+                    constants=code.co_consts,
+                    cells=code.co_cellvars,
+                    varnames=code.co_varnames,
+                    freevars=code.co_freevars,
+                    linestarts=dict(findlinestarts(code)),
+                    end_offset=offset + 10,
+                )
                 pass
             pass
         else:
             if mainfile:
                 part1 = "Python program '%s'" % mainfile
-                msg   = "is not currently running. "
-                self.msg(Mmisc.wrapped_lines(part1, msg,
-                                             self.settings['width']))
+                msg = "is not currently running. "
+                self.msg(wrapped_lines(part1, msg, self.settings["width"]))
             else:
-                self.msg('No Python program is currently running.')
+                self.msg("No Python program is currently running.")
                 pass
             self.msg(self.core.execution_status)
             pass
         return False
+
     pass
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     from trepan.processor.command import mock, info as Minfo
+
     d, cp = mock.dbg_setup()
     i = Minfo.InfoCommand(cp)
     sub = InfoPC(i)
