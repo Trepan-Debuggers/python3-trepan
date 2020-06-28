0e25e4b10476c9f7e096ddb15577842a32536c67
rocky <rocky@gnu.org>
2020-06-27 23:22:06 -0400

    Bump min uncompyle6 version

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -76,7 +76,7 @@ install_requires = [
     "pygments %s" % pygments_version,
     "spark_parser >= 1.8.9, <1.9.0",
     "tracer >= 0.3.2",
-    "uncompyle6 >= 3.7.0",
+    "uncompyle6 >= 3.7.2",
 ]
 license = "GPL3"
 mailing_list = "python-debugger@googlegroups.com"
