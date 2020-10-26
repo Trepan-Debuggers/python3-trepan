cb5d78ca840ab42f83a4fc74ad5a282a4983ecc6
rocky <rocky@gnu.org>
2020-10-26 17:45:19 -0400

    eval? now does "and" and "or"...
    By stripping off the "and" or "or"

--- trepan/processor/command/eval.py
+++ trepan/processor/command/eval.py
@@ -22,36 +22,37 @@ from trepan.lib.eval import extract_expression
 class EvalCommand(DebuggerCommand):
     """**eval** *python-statement*
 
-Run *python-statement* in the context of the current frame.
+    Run *python-statement* in the context of the current frame.
 
-If no string is given, we run the string from the current source code
-about to be run. If the command ends `?` (via an alias) and no string is
-given, the following translations occur:
+    If no string is given, we run the string from the current source code
+    about to be run. If the command ends `?` (via an alias) and no string is
+    given, the following translations occur:
 
-   assert = <expr>       => <expr>
-   {if|elif} <expr> :    => <expr>
-   while <expr> :        => <expr>
-   return <expr>         => <expr>
-   for <var> in <expr> : => <expr>
-   <var> = <expr>        => <expr>
+       assert = <expr>       => <expr>
+       {if|elif} <expr> :    => <expr>
+       while <expr> :        => <expr>
+       return <expr>         => <expr>
+       for <var> in <expr> : => <expr>
+       and <expr>            => <expr>
+       or <expr>             => <expr>
+       <var> = <expr>        => <expr>
 
-The above is done via regular expression matching. No fancy parsing is
-done, say, to look to see if *expr* is split across a line or whether
-var an assignment might have multiple variables on the left-hand side.
+    The above is done via regular expression matching. No fancy parsing is
+    done, say, to look to see if *expr* is split across a line or whether
+    var an assignment might have multiple variables on the left-hand side.
 
-Examples:
----------
+    Examples:
+    ---------
 
-    eval 1+2  # 3
-    eval      # Run current source-code line
-    eval?     # but strips off leading 'if', 'while', ..
-              # from command
+        eval 1+2  # 3
+        eval      # Run current source-code line
+        eval?     # but strips off leading 'if', 'while', ..
+                  # from command
 
-See also:
----------
+    See also:
+    ---------
 
-`deval`, `set autoeval`, `pr`, `pp` and `examine`.
-"""
+    `deval`, `set autoeval`, `pr`, `pp` and `examine`."""
 
     aliases = ("eval?", "?")
     short_help = "Print value of expression EXP"
