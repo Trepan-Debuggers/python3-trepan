113bd788ea6143a74bc6295f177ad449523b786d
rocky <rocky@gnu.org>
2021-06-17 09:49:11 -0400

    Administrative work to handle wo versions of code

--- admin-tools/make-dist-older.sh
+++ admin-tools/make-dist-older.sh
@@ -10,7 +10,10 @@ cd $(dirname ${BASH_SOURCE[0]})
 owd=$(pwd)
 trap finish EXIT
 
-if ! source ./pyenv-versions ; then
+if ! source ./pyenv-older-versions ; then
+    exit $?
+fi
+if ! source ./setup-python-3.2.sh ; then
     exit $?
 fi
 
@@ -30,9 +33,6 @@ for pyversion in $PYVERSIONS; do
     first_two=$(echo $pyversion | cut -d'.' -f 1-2 | sed -e 's/\.//')
     rm -fr build
     python setup.py bdist_egg bdist_wheel
-    # mv -v dist/${PACKAGE}-$VERSION-py${first_two_dot}.egg dist/${PACKAGE}3k-$VERSION-py${first_two_dot}.egg
-    mv -v dist/${PACKAGE}-$VERSION-py3-none-any.whl dist/${PACKAGE}-$VERSION-py${first_two}-none-any.whl
 done
 
-python ./setup.py sdist
 # mv -v dist/${PACKAGE}-$VERSION.tar.gz dist/${PACKAGE}3k-$VERSION.tar.gz
