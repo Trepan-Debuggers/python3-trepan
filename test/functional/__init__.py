a15eddf5dd2fc0774c1c8469c23dc0cab7345f25
rocky <rocky@gnu.org>
2024-02-04 10:27:49 -0500

    Add skip functional test...
    ignore speces in trace output

---
+++ test/functional/__init__.py
@@ -0,0 +1,5 @@
+"""
+Functional tests. In contrast to most unit tesets,
+these tests work with a Trepan debugger object and traces or debugs some code with it, testing some particular
+aspect of debugging, such as the ability to step, skip or jump to debugged lines, hit breakpoints, etc.
+"""
