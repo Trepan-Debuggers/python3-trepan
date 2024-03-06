11ca54ca9e23e494456505be647be11aef6c92a5
rocky <rocky@gnu.org>
2024-03-06 04:15:32 -0500

    Admnistrivia

--- admin-tools/merge-for-3.3.sh
+++ admin-tools/merge-for-3.3.sh
@@ -1,7 +1,7 @@
 #/bin/bash
-owd=$(pwd)
+trepan_merge_33_owd=$(pwd)
 cd $(dirname ${BASH_SOURCE[0]})
 if . ./setup-python-3.3.sh; then
     git merge python-3.6-to-3.10
 fi
-cd $owd
+cd $trepaan_merge_33_owd
