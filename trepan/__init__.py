5f6816f65a2a34a1dc154b14e8c5a35cddeebc01
rocky <rocky@gnu.org>
2024-07-22 11:29:44 -0400

    Get ready for release 1.2.11

--- trepan/__init__.py
+++ trepan/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#  Copyright (C) 2008-2009, 2013, 2015, 2021-2022 Rocky Bernstein <rocky@gnu.org>
+#  Copyright (C) 2008-2009, 2013, 2015, 2021-2022, 2024 Rocky Bernstein <rocky@gnu.org>
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
@@ -164,7 +164,7 @@ keeping developers happy is a good thing.(TM)
 * more testable. Much more unit and functional tests. More of _pydb_'s
   integration test will eventually be added.
 
-Copyright (C) 2008-2009, 2013-2016, 2021-2022 Rocky Bernstein <rocky@gnu.org>
+Copyright (C) 2008-2009, 2013-2016, 2021-2022, 2024 Rocky Bernstein <rocky@gnu.org>
 
 .. |Downloads| image:: https://pypip.in/download/trepan/badge.svg
 .. |Build Status| image:: https://travis-ci.org/rocky/python3-trepan.svg
