43dc8be213b92f086b11a47be9140a9c9ecb373c
rocky <rocky@gnu.org>
2024-08-25 08:02:14 -0400

    Administrivia

--- admin-tools/merge-for-3.0.sh
+++ admin-tools/merge-for-3.0.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 trepan_merge_30_owd=$(pwd)
+PYTHON_VERSION=3.12
+pyenv local $PYTHON_VERSION
 cd $(dirname ${BASH_SOURCE[0]})
 if . ./setup-python-3.0.sh; then
     git merge python-3.2-to-3.5
