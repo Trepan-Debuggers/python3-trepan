90c5908e330a23adb8e75db976535daa38dcd0ff
rocky <rocky@gnu.org>
2024-09-02 06:11:33 -0400

    Implement reloading subcommands

--- trepan/processor/command/reload.py
+++ trepan/processor/command/reload.py
@@ -19,46 +19,89 @@ import inspect
 
 # Our local modules
 from trepan.processor.command.base_cmd import DebuggerCommand
+from trepan.processor.command.base_submgr import SubcommandMgr
 
 
 class ReloadCommand(DebuggerCommand):
     """**reload** *command-name*
+       **reload** *subcommand-name* *subcommand*
 
-    Reload debugger command *command-name*.
+    Reload debugger command *command-name* or subcommand *subcommand-name* *subcommand*.
 
     This command is usually used by trepan3k developers working on the program
 
-    Example:
+    Examples:
     --------
 
         reload alias
+        reload show listsize
     """
 
     short_help = "reload a trepan3k command"
 
-    DebuggerCommand.setup(locals(), category="support", min_args=1, max_args=1)
+    DebuggerCommand.setup(locals(), category="support", min_args=1, max_args=2)
 
     def run(self, args):
         cmd_name = args[1]
         proc = self.proc
-        if cmd_name in proc.commands:
+        if len(args) == 2:
+            if cmd_name not in proc.commands:
+                self.errmsg(f'command "{cmd_name}" not found as a debugger command')
+                return
             command_module = importlib.import_module(proc.commands[cmd_name].__module__)
             importlib.reload(command_module)
-            classnames = [tup[0] for tup in inspect.getmembers(command_module, inspect.isclass) if ("DebuggerCommand" != tup[0] and tup[0].endswith("Command"))]
+            classnames = [
+                tup[0]
+                for tup in inspect.getmembers(command_module, inspect.isclass)
+                if ("DebuggerCommand" != tup[0] and tup[0].endswith("Command"))
+            ]
             if len(classnames) == 1:
                 try:
                     instance = getattr(command_module, classnames[0])(proc)
                 except Exception:
-                    print(
+                    self.errmsg(
                         f"Error loading {classnames[0]} from mod_name, sys.exc_info()[0]"
                     )
                     return
 
                 # FIXME: should we also replace object in proc.cmd_instances?
                 proc.commands[cmd_name] = instance
-                self.msg(f'reloaded command "{cmd_name}"')
+                self.msg(f'reloaded command: "{cmd_name}"')
+            pass
         else:
-            self.errmsg(f'command "{cmd_name}" not found as a debugger command"')
+            assert len(args) == 3
+            subcmd_mgr = proc.commands.get(cmd_name)
+            if subcmd_mgr is None:
+                self.errmsg(f"cannot find {cmd_name} in list of commands")
+                return
+            if not isinstance(subcmd_mgr, SubcommandMgr):
+                self.errmsg(f"command {cmd_name} does not have subcommands")
+                return
+            subcmd_name = args[2]
+            subcmd = subcmd_mgr.cmds.subcmds.get(subcmd_name)
+            if subcmd is None:
+                self.errmsg(f'command "{cmd_name}" does not have subcommand '
+                            f'"{subcmd_name}"')
+                return
+
+            subcommand_module = importlib.import_module(subcmd.__module__)
+            importlib.reload(subcommand_module)
+            classnames = [
+                tup[0] for tup in inspect.getmembers(subcommand_module, inspect.isclass)
+            ]
+            if len(classnames) == 1:
+                try:
+                    instance = getattr(subcommand_module, classnames[0])(subcmd)
+                except Exception:
+                    self.errmsg(
+                        f"Error loading {classnames[0]} from mod_name, sys.exc_info()[0]"
+                    )
+                    return
+
+                subcmd_mgr.cmds.subcmds[subcmd_name] = instance
+                self.msg(f'reloaded subcommand: "{cmd_name} {subcmd_name}"')
+            return
+
     pass
 
 
@@ -68,7 +111,12 @@ if __name__ == "__main__":
 
     dbgr, cmd = Mmock.dbg_setup()
     command = ReloadCommand(cmd)
-    for cmdline in ["reload up"]:
+    for cmdline in [
+        "reload up",
+        "reload show listsize",
+        "reload foo",
+        "reload show foo",
+    ]:
         args = cmdline.split()
         cmd_argstr = cmdline[len(args[0]) :].lstrip()
         cmd.cmd_argstr = cmd_argstr
