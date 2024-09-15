a674990e569573dc88dfc956f3b100364d7a0d26
R. Bernstein <rocky@users.noreply.github.com>
2024-09-15 10:29:06 -0400

    Use trace module_ignores (#57)

--- trepan/debugger.py
+++ trepan/debugger.py
@@ -34,10 +34,7 @@ import types
 from typing import Any, Callable
 
 import pyficache
-
-# External Egg packages
 import tracer
-
 from tracer.tracefilter import TraceFilter
 
 from trepan.exception import DebuggerQuit, DebuggerRestart
@@ -343,11 +340,15 @@ class Trepan:
     # DEFAULT_INIT_OPTS which includes references to these.
 
     # Note: has to come after functions listed in ignore_filter.
+    ignore_items = [tracer.tracer, TrepanCore]
+    trepan_debugger = sys.modules.get("trepan.debugger")
+    if trepan_debugger is not None:
+        ignore_items.append(trepan_debugger)
+    else:
+        ignore_items += [run_call, run_eval, run_script]
     DEFAULT_INIT_OPTS = {
         # What routines will we not trace into?
-        "ignore_filter": TraceFilter(
-            [tracer.start, tracer.stop, run_eval, run_call, run_eval, run_script]
-        ),
+        "ignore_filter": TraceFilter(ignore_items),
         # sys.argv when not None contains sys.argv *before* debugger
         # command processing. So sys.argv contains debugger options as
         # well as debugged-program options. These options are used to
