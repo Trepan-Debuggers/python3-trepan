127cc6b6ddf165078be343c282620fff413866cc
rocky <rocky@gnu.org>
2024-10-08 17:19:49 -0400

    Administrivia

--- admin-tools/setup-master.sh
+++ admin-tools/setup-master.sh
@@ -15,7 +15,7 @@ cd $mydir
 fulldir=$(readlink -f $mydir)
 (cd $fulldir/.. && \
      setup_version_trepan3k python-uncompyle6 master && \
-     setup_version_trepan3k xdis master && \
+     setup_version_trepan3k python-xdis master && \
      setup_version_trepan3k python-filecache master && \
      setup_version_trepan3k shell-term-background master && \
      setup_version_trepan3k pytracer master && \
