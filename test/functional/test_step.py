a701f075848f24f711556addbb6756ffaf37c282
rocky <rocky@gnu.org>
2024-11-12 02:39:10 -0500

    Adjustment for pytest on older 3.0

--- test/functional/test_step.py
+++ test/functional/test_step.py
@@ -32,8 +32,6 @@ def test_step_same_level():
     return
 
 
-@pytest.mark.skipif(PYTHON_VERSION_TRIPLE[:2] in [(3, 6)],
-                    reason="3.6 needs going over")
 def test_step_computed_value():
     # See that we can step with a computed count value
     cmds = ["step 5-3", "continue"]
