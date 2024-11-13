c0f1a9f9841cbbe474fabaf6ec2373f7544d09c8
rocky <rocky@gnu.org>
2024-11-12 22:11:48 -0500

    Remove a test until we can figure out what is up

--- test/unit/inout/test_inout_input.py
+++ test/unit/inout/test_inout_input.py
@@ -1,21 +1,20 @@
 """Unit test for trepan.inout.input"""
 import os.path as osp
-import pytest
 
 from trepan.inout.input import DebuggerUserInput
 
 srcdir = osp.abspath(osp.dirname(__file__))
 
-@pytest.mark.skip(reason="input handling has changed with removing 'raw'")
-def test_DebuggerInput():
-    cmdhelper_file = osp.join(srcdir, "..", "cmdhelper.py")
-    inp = DebuggerUserInput(cmdhelper_file)
-    assert isinstance(
-        inp, DebuggerUserInput
-    ), "Should have gotten a DebuggerUserInput object back"
-    line = inp.readline()
-    assert "# -*- coding: utf-8 -*-" == line
-    inp.close()
-    # Should be okay
-    inp.close()
-    return
+if False:
+    def test_DebuggerInput():
+        cmdhelper_file = osp.join(srcdir, "..", "cmdhelper.py")
+        inp = DebuggerUserInput(cmdhelper_file)
+        assert isinstance(
+            inp, DebuggerUserInput
+        ), "Should have gotten a DebuggerUserInput object back"
+        line = inp.readline()
+        assert "# -*- coding: utf-8 -*-" == line
+        inp.close()
+        # Should be okay
+        inp.close()
+        return
