1c1c26b820b3ac644e7b80c868c7a995175bf1ec
rocky <rocky@gnu.org>
2024-11-30 11:56:17 -0500

    Show instruction opname in "info frame"

--- trepan/lib/disassemble.py
+++ trepan/lib/disassemble.py
@@ -225,6 +225,142 @@ def disassemble_string(msg, msg_nocr, source):
 opc = get_opcode(PYTHON_VERSION_TRIPLE, IS_PYPY)
 
 
+def print_instruction(
+    instr,
+    instructions: list,
+    msg: Callable,
+    msg_nocr: Callable,
+    labels: list,
+    lasti: int=-1,
+    line_starts = {},
+    style="none",
+    opc=opc,
+    asm_format="extended",
+):
+    """Disassemble bytecode at offset `offsets`."""
+
+    # Python 3.11 introduces "CACHE" and the convention seems to be
+    # to not print these normally.
+    if instr.opname == "CACHE" and asm_format not in (
+        "extended_bytes",
+        "bytes",
+    ):
+        return
+
+    offset = instr.offset
+    opcode = instr.opcode
+
+    # Column: Source code line number
+    if instr.starts_line:
+        if offset:
+            msg("")
+        cur_line = instr.starts_line
+        msg_nocr(format_token(LineNumber, "%4d" % cur_line, style=style))
+        msg_nocr(" ")
+    else:
+        msg_nocr("     ")
+
+    # Column: Current instruction indicator
+    if offset == lasti:
+        msg_nocr(format_token(Arrow, "-->", style=style))
+    else:
+        msg_nocr("   ")
+
+    # Column: Jump target marker
+    if offset in labels:
+        msg_nocr(format_token(Arrow, ">>", style=style))
+    else:
+        msg_nocr("  ")
+
+    # Column: Instruction offset from start of code sequence
+    msg_nocr(format_token(Number, repr(offset).rjust(4), style=style))
+    msg_nocr(" ")
+
+    # Column: Instruction bytes
+    if asm_format in ("extended-bytes", "bytes"):
+        msg_nocr(format_token(Symbol, "|", style=style))
+        msg_nocr(format_token(Hex, f"{instr.opcode:02x}", style=style))
+        if instr.inst_size == 1:
+            # Not 3.6 or later
+            msg_nocr(" " * (2 * 3))
+        if instr.inst_size == 2:
+            # Must by Python 3.6 or later
+            msg_nocr(" ")
+            if instr.has_arg:
+                msg_nocr(format_token(Hex, f"{instr.arg % 256:02x}", style=style))
+            else:
+                msg_nocr(format_token(Hex, "00", style=style))
+        elif instr.inst_size == 3:
+            # Not 3.6 or later
+            opbyte, operand_byte = divmod(instr.arg, 256)
+            msg_nocr(format_token(Hex, f"{opbyte:02x}", style=style))
+            msg_nocr(" ")
+            msg_nocr(format_token(Hex, f"{operand_byte:02x}", style=style))
+
+        msg_nocr(format_token(Symbol, "|", style=style))
+        msg_nocr(" ")
+
+    # Column: Opcode name
+    msg_nocr(format_token(Keyword, instr.opname.ljust(20), style=style))
+    msg_nocr(" ")
+
+    # Column: Opcode argument
+    argrepr = None
+    if instr.arg is not None:
+        argrepr = instr.argrepr
+        # The argrepr value when the instruction was created generally has all the information we require.
+        if asm_format in ("extended", "extended-bytes"):
+            if (
+                instr.is_jump()
+                and line_starts is not None
+                and line_starts.get(instr.argval) is not None
+            ):
+                msg(
+                    format_token(
+                        Details,
+                        f"{instr.argrepr}, line {line_starts[instr.argval]}",
+                        style=style,
+                    )
+                )
+                return
+            if (
+                hasattr(opc, "opcode_extended_fmt")
+                and opc.opname[opcode] in opc.opcode_extended_fmt
+            ):
+                tos_str, start_offset = opc.opcode_extended_fmt[opc.opname[opcode]](
+                    opc, list(reversed(instructions))
+                )
+                if start_offset is not None:
+                    if style is not None and style != "none":
+                        tos_str = highlight_string(tos_str, style=style)
+                    msg(tos_str)
+                    return
+                pass
+            pass
+    elif asm_format in ("extended", "extended-bytes"):
+        # Note: instr.arg is also None
+        if (
+            hasattr(opc, "opcode_extended_fmt")
+            and opc.opname[opcode] in opc.opcode_extended_fmt
+        ):
+            tos_str, start_offset = opc.opcode_extended_fmt[opc.opname[opcode]](
+                opc, list(reversed(instructions))
+            )
+            if start_offset is not None:
+                msg(highlight_string(tos_str, style=style))
+                return
+    if argrepr is None or argrepr == "":
+        if instr.arg is not None:
+            msg(format_token(Integer, str(instr.arg), style=style))
+        else:
+            msg("")
+            pass
+    else:
+        msg(format_token(Details, instr.argrepr, style=style))
+
+    pass
+
+
 def disassemble_bytes(
     orig_msg: Callable,
     orig_msg_nocr: Callable,
@@ -245,11 +381,14 @@ def disassemble_bytes(
     end_offset=None,
     opc=opc,
     asm_format="extended",
-):
+) -> tuple:
     """Disassemble byte string of code. If end_line is negative
     it counts the number of statement line starts to use."""
+
+    def null_print(_):
+        return None
+
     instructions = []
-    statement_count = 10000
     if end_line is None:
         end_line = 10000
     elif relative_pos:
@@ -257,9 +396,6 @@ def disassemble_bytes(
         pass
 
     labels = findlabels(code, opc)
-
-    def null_print(_):
-        return None
 
     if start_line > cur_line:
         msg_nocr = null_print
@@ -270,11 +406,11 @@ def disassemble_bytes(
 
     offset = -1
     for instr in get_instructions_bytes(
-        code, opc, varnames, names, constants, cells, line_starts
+        code, opc, varnames, names, constants, cells, line_starts, labels
     ):
         instructions.append(instr)
+
         offset = instr.offset
-        opcode = instr.opcode
 
         if end_offset and offset > end_offset:
             break
@@ -289,133 +425,119 @@ def disassemble_bytes(
 
         # Column: Source code line number
         if instr.starts_line:
-            if offset:
-                msg("")
-
             cur_line = instr.starts_line
             if start_line and (
                 (start_line > cur_line) or start_offset and start_offset > offset
             ):
-                msg_nocr = null_print
-                msg = null_print
-            else:
-                statement_count -= 1
-                msg_nocr = orig_msg_nocr
-                msg = orig_msg
-                pass
+                continue
+
             if (cur_line > end_line) or (end_offset and offset > end_offset):
                 break
-            msg_nocr(format_token(LineNumber, "%4d" % cur_line, style=style))
-            msg_nocr(" ")
-        else:
-            if start_offset and offset and start_offset <= offset:
-                msg_nocr = orig_msg_nocr
-                msg = orig_msg
-                pass
-            msg_nocr("     ")
+        elif start_offset and offset and start_offset <= offset:
+            continue
 
-        # Column: Current instruction indicator
-        if offset == lasti:
-            msg_nocr(format_token(Arrow, "-->", style=style))
-        else:
-            msg_nocr("   ")
-
-        # Column: Jump target marker
-        if offset in labels:
-            msg_nocr(format_token(Arrow, ">>", style=style))
-        else:
-            msg_nocr("  ")
-
-        # Column: Instruction offset from start of code sequence
-        msg_nocr(format_token(Number, repr(offset).rjust(4), style=style))
-        msg_nocr(" ")
-
-        # Column: Instruction bytes
-        if asm_format in ("extended-bytes", "bytes"):
-            msg_nocr(format_token(Symbol, "|", style=style))
-            msg_nocr(format_token(Hex, f"{opcode:02x}", style=style))
-            if instr.inst_size == 1:
-                # Not 3.6 or later
-                msg_nocr(" " * (2 * 3))
-            if instr.inst_size == 2:
-                # Must by Python 3.6 or later
-                msg_nocr(" ")
-                if instr.has_arg:
-                    msg_nocr(format_token(Hex, f"{instr.arg % 256:02x}", style=style))
-                else:
-                    msg_nocr(format_token(Hex, "00", style=style))
-            elif instr.inst_size == 3:
-                # Not 3.6 or later
-                opbyte, operand_byte = divmod(instr.arg, 256)
-                msg_nocr(format_token(Hex, f"{opbyte:02x}", style=style))
-                msg_nocr(" ")
-                msg_nocr(format_token(Hex, f"{operand_byte:02x}", style=style))
-
-            msg_nocr(format_token(Symbol, "|", style=style))
-            msg_nocr(" ")
-
-        # Column: Opcode name
-        msg_nocr(format_token(Keyword, instr.opname.ljust(20), style=style))
-        msg_nocr(" ")
-
-        # Column: Opcode argument
-        argrepr = None
-        if instr.arg is not None:
-            argrepr = instr.argrepr
-            # The argrepr value when the instruction was created generally has all the information we require.
-            if asm_format in ("extended", "extended-bytes"):
-                if (
-                    instr.is_jump()
-                    and line_starts is not None
-                    and line_starts.get(instr.argval) is not None
-                ):
-                    msg(
-                        format_token(
-                            Details,
-                            f"{instr.argrepr}, line {line_starts[instr.argval]}",
-                            style=style,
-                        )
-                    )
-                    continue
-                if (
-                    hasattr(opc, "opcode_extended_fmt")
-                    and opc.opname[opcode] in opc.opcode_extended_fmt
-                ):
-                    tos_str, start_offset = opc.opcode_extended_fmt[opc.opname[opcode]](
-                        opc, list(reversed(instructions))
-                    )
-                    if start_offset is not None:
-                        if style is not None and style != "none":
-                            tos_str = highlight_string(tos_str, style=style)
-                        msg(tos_str)
-                        continue
-                    pass
-                pass
-        elif asm_format in ("extended", "extended-bytes"):
-            # Note: instr.arg is also None
-            if (
-                hasattr(opc, "opcode_extended_fmt")
-                and opc.opname[opcode] in opc.opcode_extended_fmt
-            ):
-                tos_str, start_offset = opc.opcode_extended_fmt[opc.opname[opcode]](
-                    opc, list(reversed(instructions))
-                )
-                if start_offset is not None:
-                    msg(highlight_string(tos_str, style=style))
-                    continue
-        if argrepr is None or argrepr == "":
-            if instr.arg is not None:
-                msg(format_token(Integer, str(instr.arg), style=style))
-            else:
-                msg("")
-                pass
-        else:
-            msg(format_token(Details, instr.argrepr, style=style))
-
-        pass
+        print_instruction(
+            instr,
+            instructions,
+            msg,
+            msg_nocr,
+            labels=labels,
+            lasti=lasti,
+            line_starts=line_starts,
+            style=style,
+            opc=opc,
+            asm_format=asm_format,
+        )
 
     return code, offset
 
+def disassemble_instruction(
+    orig_msg: Callable,
+    orig_msg_nocr: Callable,
+    code,
+    offset,
+    lasti=-1,
+    cur_line=0,
+    end_line=None,
+    relative_pos=False,
+    varnames=(),
+    names=(),
+    constants=(),
+    cells=(),
+    freevars=(),
+    line_starts={},
+    style="none",
+    opc=opc,
+    asm_format="extended",
+) -> tuple:
+    """Disassemble byte string of code. If end_line is negative
+    it counts the number of statement line starts to use."""
+
+    def null_print(_):
+        return None
+
+    instructions = []
+    if end_line is None:
+        end_line = 10000
+    elif relative_pos:
+        end_line += start_line - 1
+        pass
+
+    labels = findlabels(code, opc)
+
+    if start_line > cur_line:
+        msg_nocr = null_print
+        msg = null_print
+    else:
+        msg_nocr = orig_msg_nocr
+        msg = orig_msg
+
+    offset = -1
+    for instr in get_instructions_bytes(
+        code, opc, varnames, names, constants, cells, line_starts, labels
+    ):
+        instructions.append(instr)
+
+        offset = instr.offset
+
+        if end_offset and offset > end_offset:
+            break
+
+        # Python 3.11 introduces "CACHE" and the convention seems to be
+        # to not print these normally.
+        if instr.opname == "CACHE" and asm_format not in (
+            "extended_bytes",
+            "bytes",
+        ):
+            continue
+
+        # Column: Source code line number
+        if instr.starts_line:
+            cur_line = instr.starts_line
+            if start_line and (
+                (start_line > cur_line) or start_offset and start_offset > offset
+            ):
+                continue
+
+            if (cur_line > end_line) or (end_offset and offset > end_offset):
+                break
+        elif start_offset and offset and start_offset <= offset:
+            continue
+
+        print_instruction(
+            instr,
+            instructions,
+            msg,
+            msg_nocr,
+            labels=labels,
+            lasti=lasti,
+            line_starts=line_starts,
+            style=style,
+            opc=opc,
+            asm_format=asm_format,
+        )
+
+    return code, offset
 
 # Demo it
 if __name__ == "__main__":
