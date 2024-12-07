7adf14f82eb911d9b0308408d75f68d01e6d08f4
rocky <rocky@gnu.org>
2024-12-07 02:37:09 -0500

    Tweak pygments install ot match pyficache

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -28,7 +28,7 @@ import sys
 decompiler = "uncompyle6 >= 3.9.2"
 
 SYS_VERSION = sys.version_info[0:2]
-pygments_version = "Pygments" # I have my own patch for this
+pygments_version = "pygments>1.9,<2.1" # I have my own patch for this
 
 
 # Python-version | package  | last-version |
