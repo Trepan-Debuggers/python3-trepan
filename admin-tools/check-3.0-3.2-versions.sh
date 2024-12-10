9a5c903569f31848e85ec270fc5c48b1dc63c0e6
rocky <rocky@gnu.org>
2024-12-10 11:29:11 -0500

    Administrivia

---
+++ admin-tools/check-3.0-3.2-versions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+function finish {
+  cd $trepan3k_owd
+}
+
+# FIXME put some of the below in a common routine
+trepan3k_owd=$(pwd)
+# trap finish EXIT
+
+cd $(dirname ${BASH_SOURCE[0]})
+if ! source ./pyenv-3.0-3.2-versions ; then
+    exit $?
+fi
+
+if ! source ./setup-python-3.0.sh ; then
+    exit $?
+fi
+
+cd ..
+for version in $PYVERSIONS; do
+    if ! pyenv local $version ; then
+	exit $?
+    fi
+    python --version
+    make clean && pip install -e .
+    if ! make check; then
+	exit $?
+    fi
+    echo === $version ===
+done
+finish
