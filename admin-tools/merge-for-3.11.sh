b293acf9bb2bce943f3eb3347f000d3f7d8d2c2b
rocky <rocky@gnu.org>
2024-12-30 19:34:10 -0500

    Split off 3.11 from 3.12 and 3.13

--- admin-tools/merge-for-3.11.sh
+++ admin-tools/merge-for-3.11.sh
@@ -4,6 +4,6 @@ PYTHON_VERSION=3.6
 pyenv local $PYTHON_VERSION
 cd $(dirname ${BASH_SOURCE[0]})
 if . ./setup-python-3.6.sh; then
-    git merge python-3.11
+    git merge master
 fi
 cd $trepan_merge_36_owd
