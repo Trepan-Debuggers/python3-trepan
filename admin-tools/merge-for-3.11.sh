45d7a3434141c66ae7bb25bbbe7b6671b6df08f3
rocky <rocky@gnu.org>
2024-12-30 19:35:31 -0500

    Merge for 3.11

--- admin-tools/merge-for-3.11.sh
+++ admin-tools/merge-for-3.11.sh
@@ -1,9 +1,9 @@
 #/bin/bash
-trepan_merge_36_owd=$(pwd)
-PYTHON_VERSION=3.6
+trepan_merge_311_owd=$(pwd)
+PYTHON_VERSION=3.11
 pyenv local $PYTHON_VERSION
 cd $(dirname ${BASH_SOURCE[0]})
-if . ./setup-python-3.6.sh; then
+if . ./setup-python-3.11.sh; then
     git merge master
 fi
-cd $trepan_merge_36_owd
+cd $trepan_merge_311_owd
