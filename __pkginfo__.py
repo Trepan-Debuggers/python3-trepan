275d998d1088a61a8203c880fa66cd860f8cdfa3
rocky <rocky@gnu.org>
2025-01-15 17:03:10 -0500

    Merge hell

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2013, 2015-2018, 2020-2021, 2023-2024 Rocky Bernstein
+# Copyright (C) 2013, 2015-2018, 2020-2021, 2023-2025 Rocky Bernstein
 # <rocky@gnu.org>
 #
 #  This program is free software: you can redistribute it and/or modify
