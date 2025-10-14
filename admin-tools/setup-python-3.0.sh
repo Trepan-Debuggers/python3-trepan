486706f63c5c55a6eab1595f99889ff1f9d340b4
rocky <rocky@gnu.org>
2025-10-14 15:34:04 -0400

    branch adminitrivia

--- admin-tools/setup-python-3.0.sh
+++ admin-tools/setup-python-3.0.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # Check out 3.0-to-3.2 branch and dependent development branches
+PYTHON_VERSION=3.0
 
+trepan3k_owd=$(pwd)
 bs=${BASH_SOURCE[0]}
 if [[ $0 == $bs ]] ; then
     echo "This script should be *sourced* rather than run directly through bash"
     exit 1
 fi
 
-PYTHON_VERSION=3.0
-
-export PATH=$HOME/.pyenv/bin/pyenv:$PATH
 trepan3k_owd=$(pwd)
 mydir=$(dirname $bs)
-cd $mydir
-. ./checkout_common.sh
-(cd $mydir/../../../rocky && \
+trepan3k_fulldir=$(readlink -f $mydir)
+. $trepan3k_fulldir/checkout_common.sh
+
+(cd $trepan3k_fuilldir/../../../rocky && \
      setup_version python-uncompyle6 python-3.0 \
      setup_version python-filecache python-3.0 && \
      setup_version pytracer python-3.0 \
