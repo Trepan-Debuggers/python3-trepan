36e94f0e0b0c911a82bd2512aa485e7f7221a7e0
rocky <rocky@gnu.org>
2026-01-14 12:49:40 -0500

    More tricky fib-exec example

--- test/example/fib-exec.py
+++ test/example/fib-exec.py
@@ -3,5 +3,6 @@ def fib(x: int) -> int:
         return 1
     return fib(x-1) + fib(x-2)
 
-exec("print(fib(2))")
+command = "print(fib(2))"
+exec(command)
 print("fib(2)= %d, fib(3) = %d, fib(4) = %d\n" % (fib(2), fib(3), fib(4)))
