6d80039f22f89ba7df8f53ca607022950bcdfbc8
rocky <rocky@gnu.org>
2026-01-14 13:03:49 -0500

    Improve setting line number when switching frames

--- test/example/fib-exec.py
+++ test/example/fib-exec.py
@@ -3,6 +3,14 @@ def fib(x: int) -> int:
         return 1
     return fib(x-1) + fib(x-2)
 
-command = "print(fib(2))"
-exec(command)
+exec_command = "print(fib(2))"
+
+# Show that trepan3k can figure out what is getting called when exec is used:
+exec(exec_command)
+
+# Show that trepan3k can figure out what is getting called when eval is used:
+eval_command = "fib(0)"
+print(eval(eval_command))
+
+
 print("fib(2)= %d, fib(3) = %d, fib(4) = %d\n" % (fib(2), fib(3), fib(4)))
